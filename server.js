5fa90a3757ea47af1aa83fa1acc82d5e2e1ca302
Roxy <roxyfokker@hotmail.com>
2026-05-01 20:48:28 +0200

    feat: oude content toevoegen #10

--- server.js
+++ server.js
@@ -17,16 +17,70 @@ app.get('/', async function (request, response) {
 });
 
 app.get('/instrumenten', async function (request, response) {
-  response.render('overzicht.liquid');
+  const page = parseInt(request.query.page) || 1; //ai
+  const limit = 20; // ai
+  const offset = (page - 1) * limit; // ai
+
+  let url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/'
+  let params = new URLSearchParams();
+
+  // filteren
+  if (request.query.status) {
+    params.append('filter[status][_eq]', request.query.status);
+  }
+
+  if (request.query.type) {
+    params.append('filter[type][_eq]', request.query.type);
+  }
+
+  // sorteren
+  params.append('sort', request.query.sort || '-id');
+
+  // pagins 
+  params.append('limit', limit); // ai
+  params.append('offset', offset); // ai
+  // Totaal aantal ophalen
+  params.append('meta', 'total_count'); // ai
+  url = url + '?' + params.toString(); // ai
+
+  const instrumentsResponse = await fetch(url) 
+  const instrumentsResponseJSON = await instrumentsResponse.json()
+
+  const totalItems = instrumentsResponseJSON.meta.total_count; // ai
+  const totalPages = Math.ceil(totalItems / limit); // ai
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1); // ai
+
+  response.render('overzicht.liquid', {
+    instruments: instrumentsResponseJSON.data,
+    path: request.path,
+    status: request.query.status || null,
+    type: request.query.type || '',
+    sort: request.query.sort || null,
+    totalItems: totalItems,
+    pages: pages,
+    successName: request.query.name || null,
+  });
 });
 
 app.get('/instrumenten:id', async function (request, response) {
-    response.render('detail.liquid')
+  const url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/' + request.params.id;
+
+  const instrumentsResponse = await fetch(url) 
+  const instrumentsResponseJSON = await instrumentsResponse.json()
+
+  response.render('detail.liquid', {
+    instruments: instrumentsResponseJSON.data,
+    type: request.query.type || '',
+    actie:request.query.action || null,
+    status: request.query.status || null
+  });
 });
 
 app.get('/activiteiten', async function (request, response) {
   response.render('activiteitenlog.liquid');
 });
+
+
 
 app.post('/instrumenten', async function (request, response){
 });
