c10ea5a8ec2a7c515a76b89bfc79fc86b345a7a7
Roxy <roxyfokker@hotmail.com>
2026-05-02 12:07:51 +0200

    delete: server opnieuw opbouwen #10

--- server.js
+++ server.js
@@ -13,58 +13,33 @@ app.engine('liquid', engine.express());
 app.set('views', './views');
 
 app.get('/', async function (request, response) {
-  response.render('dashboard.liquid');
-});
-
-app.get('/instrumenten', async function (request, response) {
-  const page = parseInt(request.query.page) || 1; //ai
-  const limit = 20; // ai
-  const offset = (page - 1) * limit; // ai
-
   let url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/'
-  let params = new URLSearchParams();
-
-  // filteren
-  if (request.query.status) {
-    params.append('filter[status][_eq]', request.query.status);
-  }
-
-  if (request.query.type) {
-    params.append('filter[type][_eq]', request.query.type);
-  }
-
-  // sorteren
-  params.append('sort', request.query.sort || '-id');
-
-  // pagins 
-  params.append('limit', limit); // ai
-  params.append('offset', offset); // ai
-  // Totaal aantal ophalen
-  params.append('meta', 'total_count'); // ai
-  url = url + '?' + params.toString(); // ai
-
   const instrumentsResponse = await fetch(url) 
   const instrumentsResponseJSON = await instrumentsResponse.json()
+  const totalItems = instrumentsResponseJSON.data.length;
 
-  const totalItems = instrumentsResponseJSON.meta.total_count; // ai
-  const totalPages = Math.ceil(totalItems / limit); // ai
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1); // ai
-
-  response.render('overzicht.liquid', {
+  response.render('dashboard.liquid', {
     instruments: instrumentsResponseJSON.data,
-    path: request.path,
     status: request.query.status || null,
-    type: request.query.type || '',
-    sort: request.query.sort || null,
     totalItems: totalItems,
-    pages: pages,
-    successName: request.query.name || null,
   });
 });
 
-app.get('/instrumenten:id', async function (request, response) {
-  const url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/' + request.params.id;
+app.get('/instrumenten', async function (request, response) {
+  let url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/'
+  const instrumentsResponse = await fetch(url) 
+  const instrumentsResponseJSON = await instrumentsResponse.json()
+  const totalItems = instrumentsResponseJSON.data.length;
 
+  response.render('overzicht.liquid', {
+    instruments: instrumentsResponseJSON.data,
+    status: request.query.status || null,
+    totalItems: totalItems
+  });
+});
+
+app.get('/instrumenten/:id', async function (request, response) {
+  const url = 'https://fdnd-agency.directus.app/items/preludefonds_instruments/' + request.params.id;
   const instrumentsResponse = await fetch(url) 
   const instrumentsResponseJSON = await instrumentsResponse.json()
 
